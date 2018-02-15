49bac7ae57e88900198111548ffbb37756c74da7
voider1 <wesleyey0408@gmail.com>
2018-02-15 13:33:28 +0100

    balls

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-#!venv/bin/python
+from flask import Flask, request, jsonify
 from multiprocessing import Queue
+import random
 
-from flask import Flask
-from flask import request
+response = {
+    "version": "1.0",
+    "response": {
+        "outputSpeech": {
+            "type": "PlainText",
+            "text": "Hello World"
+        }
+    }
+}
+
+move_responses = [
+    "I'd rather not, but alright then... Going {}.",
+    "Alright, I'll go {}... But not because I like you or anything.",
+    "Okay, but I'm only going {} because I'm programmed to do so."
+]
 
 app = Flask(__name__)
 actions = Queue()
 
 
-@app.route('/test', methods=['POST'])
-def test():
-    alexa_stuff = request.get_json()
-    print(alexa_stuff)
-
-
-@app.route('/add_command', methods=['POST'])
+@app.route('/add_command', methods=['POST']) #GET requests will be blocked
 def add_command():
     req_data = request.get_json()
+    dir = req_data['request']['intent']['slots']['Direction']['value']
+    response['response']['outputSpeech']['text'] = random.choice(move_responses).format(dir)
 
-    type = req_data['type']
-    actions.put(type)
-
-    return 'added: {}'.format(type)
+    actions.put(dir)
+    return jsonify(response)
 
 
 @app.route('/get_command')
@@ -32,5 +40,12 @@ def get_command():
     return actions.get()
 
 
+@app.route('/test_alexa', methods=['POST'])
+def test_alexa():
+    print(request.get_json())
+
+    return jsonify(response)
+
+
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
